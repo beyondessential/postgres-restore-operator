9eaba63c28bcbf811f27b54888e364c8065a52c9
Félix Saparelli <felix@passcod.name>
2026-02-11 17:39:56 +1300

    show pg version of the restore

--- src/types/restore.rs
+++ src/types/restore.rs
@@ -22,6 +22,7 @@ use serde::{Deserialize, Serialize};
 	printcolumn = r#"{"name":"Replica","type":"string","jsonPath":".spec.replica.name"}"#,
 	printcolumn = r#"{"name":"Snapshot size","type":"string","jsonPath":".spec.snapshotSize"}"#,
 	printcolumn = r#"{"name":"Storage size","type":"string","jsonPath":".spec.storageSize"}"#,
+	printcolumn = r#"{"name":"Postgres version","type":"string","jsonPath":".status.postgresVersion"}"#,
 	printcolumn = r#"{"name":"Age","type":"date","jsonPath":".metadata.creationTimestamp"}"#
 )]
 #[serde(rename_all = "camelCase")]
